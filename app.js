67c28daabcaa754dce8db3f5dc5d9bd7f45ed7ee
Yeferson Gaviria <gaviriaceron001@gmail.com>
2025-08-12 18:10:36 -0500

    Crea la funcion 'limpiar()'

--- app.js
+++ app.js
@@ -4,5 +4,12 @@ function agregarAmigo(){
     let nombreAmigo = document.getElementById('amigo').value;
     listaDeNombres.push(nombreAmigo);
     console.log(listaDeNombres);
+    limpiar();
+    
 }
 
+function limpiar(){ //Limpia la caja para no tener que borrar el nombre repetidamente
+    let limpiarCaja = document.getElementById('amigo');
+    limpiarCaja.value = '';
+
+}
